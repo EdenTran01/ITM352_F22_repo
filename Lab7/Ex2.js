78573dfcb0006dcd8270ed85be7824f9319e8666
EdenTran01 <edent@hawaii.edu>
2022-09-26 20:33:33 -1000

    Update Ex2.js

--- Lab7/Ex2.js
+++ Lab7/Ex2.js
@@ -1,20 +1,21 @@
-require ("./products_data.js");
+require("./products_data.js");
 var num_products = 5;
 var prod_num = 0;
-while( prod_num++ < num_products) {
-    console.log( prod_num );
-    if(prod_num > num_products*.75 || prod_num < num_products*.75) {
-        console.log( `${eval('name' + prod_num )}`); "is sold out";
+while(prod_num++ < num_products) {
+    
+    if( (prod_num > num_products*.75) || (prod_num < num_products*.25) ) {
+        console.log(`${eval('name' + prod_num )} is sold out!`);
         continue;
     }
+    console.log( prod_num);
 }
 
-require ("./products_data.js");
-var num_products = 5;
-var prod_num = 0;
-while( prod_num++ < num_products) {
-    console.log( prod_num );
-    if(prod_num > num_products/2)
-        console.log( `That's Enough`);
-        break
-}
+// require ("./products_data.js");
+// var num_products = 5;
+// var prod_num = 0;
+// while( prod_num++ < num_products) {
+//    console.log( prod_num );
+//    if(prod_num > num_products/2)
+//        console.log( `That's Enough`);
+//        break
+// }
