639ab75416b2f571742412ac6f31374a95ac61a6
EdenTran01 <edent@hawaii.edu>
2022-10-05 23:37:31 -1000

    Update Ex6.js

--- Lab9/Ex6.js
+++ Lab9/Ex6.js
@@ -10,8 +10,7 @@ function isStringNonNegInt(q, returnErrors=false) {
     if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
     if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer    
 
-
-return (returnErrors ? errors:(errors.length ==0));
+    return (returnErrors ? errors:(errors.length ==0));
 }
 
 function checkIt(item,index){
