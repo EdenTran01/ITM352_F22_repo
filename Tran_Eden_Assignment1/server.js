352a66ee766f9ffe3e02ce607498eb85c86ed243
EdenTran01 <edent@hawaii.edu>
2022-10-26 19:53:19 -1000

    .

--- Tran_Eden_Assignment1/server.js
+++ Tran_Eden_Assignment1/server.js
@@ -1,1 +1,21 @@
 var products_array = require(__dirname + '/product_data.json');
+
+var express = require('express');
+var app = express();
+
+// Routing 
+
+// monitor all requests
+app.all('*', function (request, response, next) {
+   console.log(request.method + ' to ' + request.path);
+   next();
+});
+
+// process purchase request (validate quantities, check quantity available)
+<** your code here ***>
+
+// route all other GET requests to files in public 
+app.use(express.static(__dirname + '/public'));
+
+// start server
+app.listen(8080, () => console.log(`listening on port 8080`));
