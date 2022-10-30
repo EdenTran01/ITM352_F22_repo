fac4e8baf64483f46b4d698827c6b6c5e1e648ad
EdenTran01 <edent@hawaii.edu>
2022-10-29 14:04:38 -1000

    .

--- Tran_Eden_Assignment1/server.js
+++ Tran_Eden_Assignment1/server.js
@@ -7,14 +7,11 @@ app.use(express.urlencoded({ extended: true }));
 
 //respond to any req for any path
 app.all('*', function (request, response, next) {
-    console.log(request.method + ' to path ' + request.path);
-    next();
+   console.log(request.method + ' to ' + request.path);
+   next();
 });
 //products data from json file and stores it
-var products = require(__dirname + '/product_data.json');
-
-//to track quantity sold
-products.forEach( (prod,i) => {prod.total_sold = 0});
+var products = require(__dirname + '/products.json');
 
 //monitor requests
 app.get("/product_data.js", function (request, response, next) {
@@ -23,22 +20,30 @@ app.get("/product_data.js", function (request, response, next) {
    response.send(products_str);
 });
 
+
+
+//to track quantity sold
+products.forEach( (prod,i) => {prod.total_sold = 0});
+
+//Taken from Server Side Processing Lab Ex6 Task 2
+app.use(myParser.urlencoded({ extended: true }));
 app.post('/process_form', function (request, response) {
-    let brand = products[0]['model'];
-    let brand_price = products[0]['price']
-
-    var q = request.body['quantity_textbox'];
-    if (typeof q != 'undefined') {
-        if (isNonNegInt(q)) {
-            products[0].total_sold += Number(q);
-            response.redirect('receipt.html?quantity=' + q);
+   receipt = '';
+    let qtys = request.body[`quantity_textbox`];
+    for (i in qtys) {
+        q = qtys[i];
+        let brand = products[i]['model'];
+        let brand_price = products[i]['price'];
+        if (NonNegInt(q)) {
+            products[i]['total_sold'] += Number(q);
+            receipt += `<h3>Thank you for purchasing: ${q} ${model}. Your total is \$${q * model_price}!</h3>`; // render template string
+        } else {
+            receipt += `<h3><font color="red">${q} is not a valid quantity for ${model}!</font></h3>`;
         }
-        else {
-            response.redirect(`order_page.html?error=Invalid%20Quantity&quantity_textbox=' + q`);
-        }
-    }
-});
-
+    }});
+    response.send(receipt);
+    response.end();
+   
 app.get('/test', function (request, response, next) {
     response.send(request.method + ' to paths ' + request.path);
 });
@@ -47,16 +52,3 @@ app.use(express.static(__dirname + '/public'));
 
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
 
-function isNonNegInt(q, returnErrors = false) {
-    // Checks if a string q is a non-neg integer. If returnErrors is true, the array of errors is returned.
-    // Otherwise returns true if q is non-neg int.
-    errors = []; // assume no errors at first
-    if (q == '') q = 0;
-    if (Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
-    else {
-        if (q < 0) errors.push('Negative value!'); // Check if it is non-negative
-        if (parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
-        }
-
-    return returnErrors ? errors : (errors.length == 0);
-}
