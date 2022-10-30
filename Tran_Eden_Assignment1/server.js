afa21575e7780b652a2fe9a123a61c751e919c55
EdenTran01 <edent@hawaii.edu>
2022-10-29 15:18:11 -1000

    Update server.js

--- Tran_Eden_Assignment1/server.js
+++ Tran_Eden_Assignment1/server.js
@@ -2,6 +2,7 @@
 //Taken from Eden Tran Github Spring 2022 info_server_Ex5.js
 var express = require('express');
 var app = express();
+var qs = require("querystring")
    
 app.use(express.urlencoded({ extended: true }));
 
@@ -26,23 +27,47 @@ app.get("/product_data.js", function (request, response, next) {
 products.forEach( (prod,i) => {prod.total_sold = 0});
 
 //Taken from Server Side Processing Lab Ex6 Task 2
-app.use(myParser.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.post('/process_form', function (request, response) {
-   receipt = '';
-    let qtys = request.body[`quantity_textbox`];
-    for (i in qtys) {
-        q = qtys[i];
-        let brand = products[i]['model'];
-        let brand_price = products[i]['price'];
-        if (NonNegInt(q)) {
-            products[i]['total_sold'] += Number(q);
-            receipt += `<h3>Thank you for purchasing: ${q} ${model}. Your total is \$${q * model_price}!</h3>`; // render template string
-        } else {
-            receipt += `<h3><font color="red">${q} is not a valid quantity for ${model}!</font></h3>`;
-        }
-    }});
-    response.send(receipt);
-    response.end();
+   console.log(request.body);
+//Check if the quantities are valid
+var haserrors = false;
+var hasquantities = false;
+
+for(let i in products){
+   let q = request.body["quantity" + i];
+   //Check if all values are NonNegInt or Quantities
+   haserrors = haserrors || (NonNegInt (q) == false);
+   //Check if quantites asked for are available
+   haserrors = haserrors || (q > products[i].quantity_available);
+   //Check if quantity > 0
+   hasquantities = hasquantities || (q > 0);
+   console.log(hasquantities, haserrors);
+}
+
+//Check if any quantities were selected
+haserrors = haserrors || (hasquantities == false)
+
+
+
+
+//if there are no errors, then redirect to invoice with the quantities desired
+var qstring = qs.stringify(request.body);
+if(haserrors == false){
+   response.redirect("./invoice.html?" + qstring);
+}
+else{
+   response.redirect("./products_display.html?" + qstring)
+}
+
+
+
+
+
+
+
+    });
+
    
 app.get('/test', function (request, response, next) {
     response.send(request.method + ' to paths ' + request.path);
@@ -52,3 +77,14 @@ app.use(express.static(__dirname + '/public'));
 
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback
 
+function NonNegInt(arrayElement, returnErrors = false) {
+   errors = []; // assume no errors at first
+   if(arrayElement == "") {
+      arrayElement = 0;
+   }
+   if (Number(arrayElement) != arrayElement) errors.push('Not a number!'); // Check if string is a number value
+   if (arrayElement < 0) errors.push('Negative value!'); // Check if it is non-negative
+   if (parseInt(arrayElement) != arrayElement) errors.push('Not an integer!'); // Check that it is an integer
+
+   return (returnErrors ? errors : (errors.length == 0));
+}
