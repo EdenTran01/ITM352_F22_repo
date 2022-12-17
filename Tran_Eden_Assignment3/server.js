e1a29cab6bd7616220ed07c0b22b1487e2b2ac93
EdenTran01 <edent@hawaii.edu>
2022-12-16 15:36:32 -1000

    .

--- Tran_Eden_Assignment3/server.js
+++ Tran_Eden_Assignment3/server.js
@@ -5,7 +5,7 @@ var app = express();
 var qs = require("querystring");
 
 var session = require('express-session');
-app.use(session({secret: "MySecretKey", resave: true, saveUninitialized: true})); 
+app.use(session({ secret: "MySecretKey", resave: true, saveUninitialized: true }));
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -15,7 +15,7 @@ var filename = __dirname + '/user_data.json';
 
 //used to store quantity data from products disiplay page
 //assume empty at first
-var current_users= {};
+var current_users = {};
 
 // Create an object to keep count of how many users are logged in
 var status = [];
@@ -36,13 +36,13 @@ if (fs.existsSync(filename)) {
 app.all('*', function (request, response, next) {
    console.log(request.method + ' to ' + request.path);
    //Taken from Assignment 3 Code Class Example
-   if(typeof request.session.cart == 'undefined'){ 
-   request.session.cart = {};
+   if (typeof request.session.cart == 'undefined') {
+      request.session.cart = {};
       for (let pkey in allproducts) {
-         request.session.cart[pkey] = Array(allproducts[pkey].length).fill( 0 );
+         request.session.cart[pkey] = Array(allproducts[pkey].length).fill(0);
       }
-     
-   } 
+
+   }
    console.log(request.session.cart);
    next();
 });
@@ -60,8 +60,8 @@ app.get("/products_data.js", function (request, response, next) {
 //Cart Add
 //Taken from Assignment 3 Eden Tran Spring Github
 app.post("/get_cart", function (request, response) {
-    response.json(request.session.cart);
-} 
+   response.json(request.session.cart);
+}
 );
 
 //monitor requests
@@ -75,13 +75,13 @@ app.get("/current_users", function (request, response, next) {
 //monitors requests and sends user data to check if the user has logged in successfully
 //if successful, stores the user data into query string and displays it on invocie page
 app.get("/invoice.html", function (request, response, next) {
-   if(typeof request.query['email'] != 'undefined'){
-      if(user_data[request.query['email']].loggedin == true){
+   if (typeof request.query['email'] != 'undefined') {
+      if (user_data[request.query['email']].loggedin == true) {
          next();
          return;
       }
 
-      
+
    }
    //redirects to login if login information is not valid/true
    response.redirect("./login.html?" + qs.stringify(request.query));
@@ -98,7 +98,7 @@ app.post('/addtocart', function (request, response) {
    var hasquantities = false;
    var prod_key = request.body.prod_key
 
-   for (let i in allproducts[prod_key]) { 
+   for (let i in allproducts[prod_key]) {
       let q = request.body["Quantity" + i];
 
       //Check if quantity > 0
@@ -118,10 +118,10 @@ app.post('/addtocart', function (request, response) {
          //Referenced from Lab 12
          //Remove item sold from inventory
 
-        //add selected items to cart in session
-        for (let i in allproducts[prod_key]) {
-         request.session.cart[prod_key][i] += Number(request.body["Quantity" + i]);
-        }
+         //add selected items to cart in session
+         for (let i in allproducts[prod_key]) {
+            request.session.cart[prod_key][i] += Number(request.body["Quantity" + i]);
+         }
          response.redirect("./cart.html?" + qs.stringify(request.body));
       } else {
          //User will be kept on the page if the input is invalid
@@ -213,59 +213,59 @@ app.post("/register", function (request, response) {
 
    //Require a specific email format
    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(request.body.email) == false) {
-       errors.push('Please enter a valid email address');
+      errors.push('Please enter a valid email address');
    } else if (request.body.email.length == 0) {
-       errors.push('Please enter an email'); 
+      errors.push('Please enter an email');
    }
 
    //if a user's email is not undefined meanning that it is already declared, this error will display
-   if (typeof user_data[new_email] != 'undefined') { 
-       errors.push('Email is already taken.');
+   if (typeof user_data[new_email] != 'undefined') {
+      errors.push('Email is already taken.');
    }
 
    //Name Check
    if (typeof request.body.name != 'undefined') {
-       if (/^[A-Za-z ]+$/.test(request.body.name) == false) {
-           errors.push('Please enter a valid name.'); 
-       }
+      if (/^[A-Za-z ]+$/.test(request.body.name) == false) {
+         errors.push('Please enter a valid name.');
+      }
    } else if (request.body.name.length == 0) {
-       errors.push('Please enter a name.');
+      errors.push('Please enter a name.');
    }
 
    //if a user's password does not have at least 10 characters, this error will display
    if (request.body.new_password.length < 10) {
-       errors.push('Password must have a minimum of 10 characters.');
+      errors.push('Password must have a minimum of 10 characters.');
    }
 
    //if a user's password and repeat password do not match, this error will display
    if (request.body.new_password !== request.body.repeat_password) {
-       errors.push('Passwords must match');
+      errors.push('Passwords must match');
    }
 
    let params = new URLSearchParams(request.query);
 
    //If errors is empty
    if (JSON.stringify(errors) == '[]') {
-       //Write data and send to invoice.html
-       user_data[new_email] = {};
-       user_data[new_email].name = request.body.name;
-       user_data[new_email].password = request.body.new_password;
-       user_data[new_email]['status'] = "loggedin"
+      //Write data and send to invoice.html
+      user_data[new_email] = {};
+      user_data[new_email].name = request.body.name;
+      user_data[new_email].password = request.body.new_password;
+      user_data[new_email]['status'] = "loggedin"
 
-       //Writes user information into file
-       fs.writeFileSync(filename, JSON.stringify(user_data), "utf-8");
+      //Writes user information into file
+      fs.writeFileSync(filename, JSON.stringify(user_data), "utf-8");
 
-       //Add email to query
-       params.append('email', request.body.email);
-       response.redirect('./login.html?' + params.toString());
-       return;
+      //Add email to query
+      params.append('email', request.body.email);
+      response.redirect('./login.html?' + params.toString());
+      return;
    }
    else {
-       //If there are errors, send back to register page with errors
-       request.query['email'] = email;
-       request.query['name'] = name;
-       request.query['errors'] = JSON.stringify(errors);
-       response.redirect(`./registration.html?` + qs.stringify(request.query));
+      //If there are errors, send back to register page with errors
+      request.query['email'] = email;
+      request.query['name'] = name;
+      request.query['errors'] = JSON.stringify(errors);
+      response.redirect(`./registration.html?` + qs.stringify(request.query));
    }
 });
 
@@ -280,52 +280,52 @@ app.post("/updatepwd", function (request, response) {
 
    //Check if email exists
    if (typeof user_data[login_email] != 'undefined') {
-       //Then checks password entered matches stored password
-       if (user_data[login_email].password == login_password) {
+      //Then checks password entered matches stored password
+      if (user_data[login_email].password == login_password) {
 
-           //Require a minimum of 8 characters
-           if (request.body.new_password.length < 10) {
-               errors.push('Password must have a minimum of 8 characters.');
-           }
+         //Require a minimum of 8 characters
+         if (request.body.new_password.length < 10) {
+            errors.push('Password must have a minimum of 8 characters.');
+         }
 
-           //Confirm that both passwords were entered correctly
-           if (request.body.new_password !== request.body.repeat_new_password) {
-               errors.push('Passwords must match');
-           }
-           if (request.body.new_password == login_password) {
-               errors.push('New password cannot be the same as old password');
-           }
+         //Confirm that both passwords were entered correctly
+         if (request.body.new_password !== request.body.repeat_new_password) {
+            errors.push('Passwords must match');
+         }
+         if (request.body.new_password == login_password) {
+            errors.push('New password cannot be the same as old password');
+         }
 
-           if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(request.body.email) == false) {
-               errors.push('Please enter a valid email address');
-           } else if (request.body.email.length == 0) {
-               errors.push('Please enter an email');
-           }
+         if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(request.body.email) == false) {
+            errors.push('Please enter a valid email address');
+         } else if (request.body.email.length == 0) {
+            errors.push('Please enter an email');
+         }
 
-           let params = new URLSearchParams(request.query);
+         let params = new URLSearchParams(request.query);
 
-           //If errors is empty
-           if (JSON.stringify(errors) == '[]') {
-               //Write data and send to invoice.html
-               user_data[login_email].password = request.body.new_password;
+         //If errors is empty
+         if (JSON.stringify(errors) == '[]') {
+            //Write data and send to invoice.html
+            user_data[login_email].password = request.body.new_password;
 
-               //Writes user information into file
-               fs.writeFileSync(filename, JSON.stringify(user_data), "utf-8");
+            //Writes user information into file
+            fs.writeFileSync(filename, JSON.stringify(user_data), "utf-8");
 
-               //Add email to query
-               params.append('email', request.body.email);
-               response.redirect('./login.html?' + params.toString());
-               return;
-           }
-       }
-       else {
-           //If password is incorrect
-           errors.push('Incorrect password');
-       }
+            //Add email to query
+            params.append('email', request.body.email);
+            response.redirect('./login.html?' + params.toString());
+            return;
+         }
+      }
+      else {
+         //If password is incorrect
+         errors.push('Incorrect password');
+      }
    }
    else {
-       //If email has not been created
-       errors.push(`${login_email} does not exist`);
+      //If email has not been created
+      errors.push(`${login_email} does not exist`);
    }
 
    //If there are errors, send back to new password page with errors
@@ -353,10 +353,57 @@ app.post('/process_logout', function (request, response) {
 
 //--------------------Cart--------------------//
 
-        /*
-         for (let i in allproducts) {
-            let q = request.body["Quantity" + i];
-            var remainder = products[i].quantity_available;
-            products[i].quantity_available = remainder - Number(q);             
-         }
-         */
+   app.get("/checkout", function (request, response) {
+
+      for (let i in allproducts) {
+         let q = request.body["Quantity" + i];
+         var remainder = allproducts[i].quantity_available;
+         allproducts[i].quantity_available = remainder - Number(q);             
+      }
+
+      //Taken from Assignment 3 Code Examples
+      // Generate HTML invoice string
+        var invoice_str = `Thank you for your order!<table border><th>Quantity</th><th>Item</th>`;
+        var shopping_cart = request.session.cart;
+        for(products_key in products_data) {
+          for(i=0; i<products_data[products_key].length; i++) {
+              if(typeof shopping_cart[producs_key] == 'undefined') continue;
+              qty = shopping_cart[products_key][i];
+              if(cart[products_key][i] > 0) {
+                invoice_str += `<tr><td>${cart[products_key][i]}</td><td>${products_data[products_key][i].model}</td><tr>`;
+              }
+          }
+      }
+        invoice_str += '</table>';
+      // Set up mail server. Only will work on UH Network due to security restrictions
+        var transporter = nodemailer.createTransport({
+          host: "mail.hawaii.edu",
+          port: 25,
+          secure: false, // use TLS
+          tls: {
+            // do not fail on invalid certs
+            rejectUnauthorized: false
+          }
+        });
+      
+        var user_email = 'edent@hawaii.edu';
+        var mailOptions = {
+          from: 'phoney_store@bogus.com',
+          to: user_email,
+          subject: 'Your phoney invoice',
+          html: invoice_str
+        };
+      
+        transporter.sendMail(mailOptions, function(error, info){
+          if (error) {
+            invoice_str += '<br>There was an error and your invoice could not be emailed :(';
+          } else {
+            invoice_str += `<br>Your invoice was mailed to ${user_email}`;
+          }
+          response.send(invoice_str);
+          // Send to invoice page 
+         response.redirect('/invoice.html?' + qs.stringify(request.query));
+        });
+       
+      });
+ 
